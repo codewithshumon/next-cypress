ab7cf44ebb627c791f25559da55a925aacf96c7b
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-12-13 18:12:00 +0600

    Adding 4th section to landing page to show Testimonials

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image';
+import { randomUUID } from 'crypto';
+
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
 
 import Banner from '../../../public/appBanner.png';
 import Cal from '../../../public/cal.png';
 
 import TitleSection from '@/components/landingPage/TitleSection';
+import { CustomCard } from '@/components/landingPage/CustomCard';
 import { Button } from '@/components/ui/button';
-import { CLIENTS } from '@/lib/constants';
+import { CLIENTS, USERS } from '@/lib/constants';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { CardDescription, CardTitle } from '@/components/ui/card';
 
 export default function HomePage() {
   return (
@@ -175,6 +182,88 @@ export default function HomePage() {
         "
         >
           <Image src={Cal} alt="Banner" className="rounded-2xl" />
+        </div>
+      </section>
+      <section className="relative">
+        <div
+          className="w-full
+          blur-[120px]
+          rounded-full
+          h-32
+          absolute
+          bg-brand-primaryPurple/50
+          -z-100
+          top-56
+        "
+        />
+        <div
+          className="mt-20
+          px-4
+          sm:px-6 
+          flex
+          flex-col
+          overflow-x-hidden
+          overflow-visible
+        "
+        >
+          <TitleSection
+            title="Trusted by all"
+            subheading="Join thousands of satisfied users who rely on our platform for their 
+            personal and professional productivity needs."
+            pill="Testimonials"
+          />
+          {[...Array(2)].map((arr, index) => (
+            <div
+              key={randomUUID()}
+              className={twMerge(
+                clsx('mt-10 flex flex-nowrap gap-6 self-start', {
+                  'flex-row-reverse': index === 1,
+                  'animate-[slide_250s_linear_infinite]': true,
+                  'animate-[slide_250s_linear_infinite_reverse]': index === 1,
+                  'ml-[100vw]': index === 1,
+                }),
+                'hover:paused'
+              )}
+            >
+              {USERS.map((testimonial, index) => (
+                <CustomCard
+                  key={testimonial.name}
+                  className="w-[500px]
+                  shrink-0s
+                  rounded-xl
+                  dark:bg-gradient-to-t
+                  dark:from-border dark:to-background
+                "
+                  cardHeader={
+                    <div
+                      className="flex
+                      items-center
+                      gap-4
+                  "
+                    >
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <CardTitle className="text-foreground">
+                          {testimonial.name}
+                        </CardTitle>
+                        <CardDescription className="dark:text-washed-purple-800">
+                          {testimonial.name.toLocaleLowerCase()}
+                        </CardDescription>
+                      </div>
+                    </div>
+                  }
+                  cardContent={
+                    <p className="dark:text-washed-purple-800">
+                      {testimonial.message}
+                    </p>
+                  }
+                ></CustomCard>
+              ))}
+            </div>
+          ))}
         </div>
       </section>
     </>
---
+++ src/components/landingPage/CustomCard.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils';
+import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
+
+type CardProps = React.ComponentProps<typeof Card>;
+
+type CustomCardProps = CardProps & {
+  cardHeader?: React.ReactNode;
+  cardContent?: React.ReactNode;
+  cardFooter?: React.ReactNode;
+};
+export const CustomCard: React.FC<CustomCardProps> = ({
+  className,
+  cardHeader,
+  cardContent,
+  cardFooter,
+  ...props
+}) => {
+  return (
+    <Card className={cn('w-[380px', className)} {...props}>
+      <CardHeader>{cardHeader}</CardHeader>
+      <CardContent className="grid gap-4">{cardContent}</CardContent>
+      <CardFooter>{cardFooter}</CardFooter>
+    </Card>
+  );
+};
